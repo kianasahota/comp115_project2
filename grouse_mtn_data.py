8f604fad35aadfa29525a95ac1fc3a7eb73a3cb5
kianasahota <kianasahota@gmail.com>
2026-04-08 22:11:00 -0700

    Add files via upload

--- grouse_mtn_data.py
+++ grouse_mtn_data.py
@@ -1,14 +1,24 @@
 
 from pathlib import Path 
-import csv 
+#imports pathlib class from Path module 
 
-import matplotlib.pyplot as plt
+import csv               
+#imports csv module 
 
-from datetime import datetime
+import matplotlib.pyplot as plt 
+#imports matplotlib.pyplot module but shortens pyplot to plt 
+
+from datetime import datetime  
+#imports datetime class from datetime module 
 
 #importing data from csv file
-path = Path('project_2.py/grouse_climate_data.csv')
+path = Path('project_2.py/grouse_climate_data.csv') 
+
+#path.read_text() opens the csv file and puts all the text into python as one big string
+#splitlines() splits the big string into lines of smaller strings when encounters '\n' (newline character)
 lines = path.read_text().splitlines()
+
+#csv.reader() breaks lines whenever it sees a comma so that it creates columns 
 reader = csv.reader(lines)
 header_row = next(reader)
 print(header_row)
@@ -45,13 +55,13 @@ ax.plot(dates, lows, color = 'blue', alpha = 0.5, label = 'Low Temperatures')
 ax.fill_between(dates, highs, lows, facecolor = 'blue', alpha = 0.1, label = 'Daily Range')
 
 #fomratting
-ax.set_title("Daily High and Low Temperatures on Grouse Mountain 2026", fontsize = 20)
+ax.set_title("Daily High and Low Temperatures on Grouse Mountain 2026", fontsize = 20) #\n
 ax.set_xlabel('Date in 2026', fontsize = 16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (C)",fontsize=16)
 ax.tick_params(labelsize=12)
 
-ax.legend(loc = 'upper right', bbox_to_anchor = (0.9, 1.0), fontsize = 12, shadow = True )
+ax.legend(loc = 'upper right', bbox_to_anchor = (0.9, 1.0), fontsize = 12, shadow = True ) #ax.legend(0r)
 
 #Create grid in background 
 ax.grid(True, linestyle='--', alpha=0.6)
